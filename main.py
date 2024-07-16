a6a32b87698290ac764c5c2fca96a422186be669
kizkiz <yetislayer7@hotmail.com>
2024-07-16 20:22:42 +1000

    Add more pages using range

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv('topics.csv')
 
 for index, row in df.iterrows():
+
     pdf.add_page()
 
     pdf.set_font(family="Times", style="B", size=24)
@@ -13,6 +14,9 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1)
     pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 pdf.output("output.pdf")
 
