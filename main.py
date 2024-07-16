e330db32b42fe82f04da6f4b7d409b2bc571c65f
kizkiz <yetislayer7@hotmail.com>
2024-07-17 06:24:53 +1000

    Add lines to all pages using range

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
     pdf.ln(265)
@@ -30,6 +32,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
 
